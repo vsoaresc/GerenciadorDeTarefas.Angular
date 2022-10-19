c1f2a4025cfa8fb0d8e2869802aa138306fabe38
Vitor Soares <58884615+vsoaresc@users.noreply.github.com>
2022-10-18 23:18:16 -0300

    roteamento tarefa listar

--- gerenciador-de-tarefas/src/app/app-routing.module.ts
+++ gerenciador-de-tarefas/src/app/app-routing.module.ts
@@ -1,8 +1,14 @@
 import {NgModule} from '@angular/core';
 import {Routes, RouterModule} from '@angular/router';
+import { ListarTarefaComponent, TarefaRoutes } from './tarefas';
 
 export const routes: Routes = [
-
+    {
+      path: '',
+      redirectTo: 'tarefas/listar',
+      pathMatch: 'full'
+    },
+    ...TarefaRoutes
 ];
 
 @NgModule({
--- gerenciador-de-tarefas/src/app/tarefas/tarefas.module.ts
+++ gerenciador-de-tarefas/src/app/tarefas/tarefas.module.ts
@@ -1,18 +1,24 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { ListarTarefaComponent } from './listar';
+import { TarefaService } from './shared';
 
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    ListarTarefaComponent
+  ],
   imports: [
     CommonModule
   ],
+  providers:[
+    TarefaService
+  ]
 
 })
 export class TarefasModule {
 
   constructor() {}
-
 
 }
